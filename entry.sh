7c7cbe9a985cb9877d2a7abb63e44343350b3785
Kola9INE <lawalkolawole902@gmail.com>
2024-10-03 06:36:43 +0100

    BUG FIX: modified the git bash script

--- entry.sh
+++ entry.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 echo "I'm to open a streamlit app in local host"
 cd "C:\Users\HP\Desktop\guest_entry"
-cd "temp_guest"
 echo "Activating environment..."
-cd 'Scripts'
-activate
-echo "Going back to entry folder"
-cd ../..
-cd 'guest_ms'
-streamlit run entry.py
+source temp_guest/Scripts/activate
+cd guest_ms
+streamlit run bravaFD.py
