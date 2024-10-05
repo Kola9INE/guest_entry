8be9a10d2830047ceed8ba47cfc936bbd17b402c
Lawal Kolawole <142318756+Kola9INE@users.noreply.github.com>
2024-10-05 22:39:17 +0100

    feat: update the entry.sh
    Direct the shell script to navigate the directory as it is on the github repe

--- entry.sh
+++ entry.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Let our guests experience what we call comfort"
-cd "C:\Users\HP\Desktop\FRONT_DESK_OPS"
 echo "Activating environment..."
 source temp_guest/Scripts/activate
 cd guest_ms
